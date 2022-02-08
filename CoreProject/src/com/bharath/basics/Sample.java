caaf7c4dc7cd2db179e88d2385bf847dc0f6ba30
Bharath <39233427+SANDHARI@users.noreply.github.com>
2022-02-08 10:03:56 +0530

    Commit from Neon

---
+++ CoreProject/src/com/bharath/basics/Sample.java
@@ -0,0 +1,9 @@
+package com.bharath.basics;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Bharath");
+	}
+
+}
---
+++ CoreProject/src/com/bharath/basics/Sample1.java
@@ -0,0 +1,15 @@
+package com.bharath.basics;
+
+public class Sample1 {
+	
+	void show(){
+		
+		System.out.println("Hello QEdge");
+	}
+	
+	public static void main(String[] args) {
+		Sample1 s= new Sample1();
+		s.show();
+	}
+
+}
